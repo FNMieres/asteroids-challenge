c24bf97005955e8bfd6827c6f3206463497c101e
Federico Mieres <fnmieres@gmail.com>
2023-08-25 01:17:44 +0200

    feat: asteroids list pagination

--- client/src/features/asteroids/components/AsteroidsList.tsx
+++ client/src/features/asteroids/components/AsteroidsList.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 import { AsteroidElement } from "../../../types";
 import AsteroidCard from "../../asteroid/components/AsteroidCard";
 import { useAppDispatch } from "../../../app/hooks";
@@ -7,12 +9,22 @@ import {
   removeAsteroidFromFavorites,
 } from "../../asteroid/asteroidSlice";
 
+const itemsPerPage = 5;
+
 interface AsteroidsListProps {
   asteroids: AsteroidElement[] | null;
 }
 
 function AsteroidsList({ asteroids }: AsteroidsListProps) {
   const dispatch = useAppDispatch();
+
+  const [visibleAsteroids, setVisibleAsteroids] = useState(
+    asteroids?.slice(0, itemsPerPage) || [],
+  );
+
+  useEffect(() => {
+    setVisibleAsteroids(asteroids?.slice(0, itemsPerPage) || []);
+  }, [asteroids]);
 
   const toggleFavorite = (id: string) => {
     const asteroidFound = asteroids?.find((asteroid) => asteroid.id === id);
@@ -25,25 +37,41 @@ function AsteroidsList({ asteroids }: AsteroidsListProps) {
     }
   };
 
+  const handleSeeMore = () => {
+    const nextIndex = visibleAsteroids.length + itemsPerPage;
+    const nextAsteroids = asteroids?.slice(0, nextIndex) || [];
+    setVisibleAsteroids(nextAsteroids);
+  };
+
   return (
     <Stack spacing={4}>
-      {asteroids &&
-        asteroids.map((asteroid) => (
-          <AsteroidCard
-            key={asteroid.id}
-            id={asteroid.id}
-            name={asteroid.name}
-            date={asteroid.close_approach_data[0].close_approach_date}
-            diameter={asteroid.estimated_diameter.meters.estimated_diameter_max}
-            velocity={
-              asteroid.close_approach_data[0].relative_velocity
-                .kilometers_per_hour
-            }
-            isHazardous={asteroid.is_potentially_hazardous_asteroid}
-            isFavorite={asteroid.favorite}
-            onClickFavorite={toggleFavorite}
-          />
-        ))}
+      {asteroids && (
+        <>
+          {visibleAsteroids.map((asteroid) => (
+            <AsteroidCard
+              key={asteroid.id}
+              id={asteroid.id}
+              name={asteroid.name}
+              date={asteroid.close_approach_data[0].close_approach_date}
+              diameter={
+                asteroid.estimated_diameter.meters.estimated_diameter_max
+              }
+              velocity={
+                asteroid.close_approach_data[0].relative_velocity
+                  .kilometers_per_hour
+              }
+              isHazardous={asteroid.is_potentially_hazardous_asteroid}
+              isFavorite={asteroid.favorite}
+              onClickFavorite={toggleFavorite}
+            />
+          ))}
+          {asteroids && visibleAsteroids.length < asteroids.length && (
+            <Button variant="contained" onClick={handleSeeMore}>
+              See More
+            </Button>
+          )}
+        </>
+      )}
     </Stack>
   );
 }
